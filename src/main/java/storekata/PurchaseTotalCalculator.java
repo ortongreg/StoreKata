10fa5665f5eeb93bfc0627d4f578cf18d6c92e55
Greg Orton <greg.a.orton@accenture.com>
2021-02-06 14:01:04 -0600

    Refactor PurchaseTotalCalculator, pulled extracted rounding method for clarity

--- src/main/java/storekata/PurchaseTotalCalculator.java
+++ src/main/java/storekata/PurchaseTotalCalculator.java
@@ -7,6 +7,10 @@ import java.util.List;
 public class PurchaseTotalCalculator {
     public double calculatePurchaseTotal(List<Item> items){
         double sum = items.stream().mapToDouble(Item::getCost).sum();
+        return roundToCents(sum);
+    }
+
+    private double roundToCents(double sum) {
         return (double) Math.round(sum * 100) / 100;
     }
 }
