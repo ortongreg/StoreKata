d635319fb2c0aa6c637240fd6959b64e8e735442
Greg Orton <greg.a.orton@accenture.com>
2021-02-06 13:52:00 -0600

    PurchaseTotalCalculator can sum total of multiple items that cost 1.23

--- src/main/java/storekata/PurchaseTotalCalculator.java
+++ src/main/java/storekata/PurchaseTotalCalculator.java
@@ -5,5 +5,7 @@ import storekata.models.Item;
 import java.util.List;
 
 public class PurchaseTotalCalculator {
-    public double calculatePurchaseTotal(List<Item> items){ return 1.23;}
+    public double calculatePurchaseTotal(List<Item> items){
+        return 1.23 * items.size();
+    }
 }
--- src/test/java/storekata/PurchaseTotalCalculatorTests.java
+++ src/test/java/storekata/PurchaseTotalCalculatorTests.java
@@ -3,6 +3,7 @@ package storekata;
 import org.junit.jupiter.api.Test;
 import storekata.models.Item;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,5 +15,13 @@ public class PurchaseTotalCalculatorTests {
     public void GivenOneItem_WhenCalculatePurchaseTotal_ThenReturnsThatItemCost(){
         double cost = calculator.calculatePurchaseTotal(Collections.singletonList(new Item("Foo")));
         assertEquals(1.23, cost);
+    }
+
+    @Test
+    public void GivenMultipleOfSameItem_WhenCalculatePurchaseTotal_ThenReturnsSum(){
+        double cost = calculator.calculatePurchaseTotal(Arrays.asList(
+                new Item("Foo"), new Item("Foo")
+        ));
+        assertEquals(2.46, cost);
     }
 }
